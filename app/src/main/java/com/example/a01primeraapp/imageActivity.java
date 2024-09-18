93391c6a2cbd77ffb17ef392b699aef4d6bb9c1d
batistaker47 <david.pires1997@gmail.com>
2024-09-18 14:23:19 +0200

    Funcionalidad de cambio de color de fondo añadida

--- app/src/main/java/com/example/a01primeraapp/imageActivity.java
+++ app/src/main/java/com/example/a01primeraapp/imageActivity.java
@@ -1,11 +1,14 @@
 package com.example.a01primeraapp;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RelativeLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -26,5 +29,10 @@ public class imageActivity extends AppCompatActivity {
 
     public void changeToHome(View view) {
         startActivity(new Intent (imageActivity.this, MainActivity.class));
+    }
+
+    public void changeBGColor(View view) {
+        ConstraintLayout layout = findViewById(R.id.main);
+        layout.setBackgroundColor(Color.BLACK);
     }
 }
