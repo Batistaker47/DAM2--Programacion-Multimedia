4484cf54c45f50e3f771fb943ca9f1d2fda86018
batistaker47 <david.pires1997@gmail.com>
2024-09-18 13:32:46 +0200

    Primera Actividad comenzada. Avances hasta el logcat

---
+++ app/src/main/java/com/example/a01primeraapp/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.a01primeraapp;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button changeToImage = findViewById(R.id.button_homeToActivityImage);
+
+        // EL evento onClickListener nos vale para decir qué evento va a ocurrir cuando pulsemos el botón
+
+        changeToImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Pulsado!", Toast.LENGTH_SHORT).show();
+            Log.d("debug","OKEY");
+            }
+        });
+    }
+}
