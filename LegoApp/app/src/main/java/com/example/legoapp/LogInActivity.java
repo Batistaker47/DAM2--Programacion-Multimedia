6363c6d0899518c2aebb7ac6696a224bbfbacf73
DvdPires19 <david.pires1997@gmail.com>
2024-10-06 18:21:00 +0200

    LegoApp created. Sign in page created and still under review.

---
+++ LegoApp/app/src/main/java/com/example/legoapp/LogInActivity.java
@@ -0,0 +1,31 @@
+package com.example.legoapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LogInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    public void changeToMainView(View view) {
+        startActivity(new Intent(LogInActivity.this, MainPageActivity.class));
+        Toast.makeText(this, "Welcome User", Toast.LENGTH_LONG).show();
+    }
+}
