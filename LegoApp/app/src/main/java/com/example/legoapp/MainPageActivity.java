801d1eff87af64d1c4de8fe1a60f1065498e78c5
batistaker47 <david.pires1997@gmail.com>
2024-10-09 14:13:32 +0200

    Firebase configurated

--- LegoApp/app/src/main/java/com/example/legoapp/MainPageActivity.java
+++ LegoApp/app/src/main/java/com/example/legoapp/MainPageActivity.java
@@ -20,5 +20,36 @@ public class MainPageActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
     }
+
+    /*public void addToFirebase_DB(View view) {
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        Map<String, Object> values = new HashMap<>();
+        TextView username = findViewById(R.id.nameTextView);
+        TextView email = findViewById(R.id.mailTextView);
+        TextView password = findViewById(R.id.passTextView);
+
+        values.put("userame",username.getText().toString());
+        values.put("email",email.getText().toString());
+        values.put("password",password.getText().toString());
+
+        // Añadir nuevo documento (habría que hacer un control de errores para comporbar si ya existe)
+        database.collection("users").document(username.getText().toString())
+                .set(values)
+                //
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Toast.makeText(MainActivity.this,"Todo Ok",Toast.LENGTH_LONG).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this,"ERROR",Toast.LENGTH_LONG).show();
+
+                    }
+                });
+    }*/
 }
