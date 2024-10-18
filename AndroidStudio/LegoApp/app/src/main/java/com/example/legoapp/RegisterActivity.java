a2d88d92a08d237c5f0285e164486e4b721f72b7
DvdPires19 <david.pires1997@gmail.com>
2024-10-18 20:38:54 +0200

    Register page modified. Creation of new users in Firebase already working. Layout design modified

--- AndroidStudio/LegoApp/app/src/main/java/com/example/legoapp/RegisterActivity.java
+++ AndroidStudio/LegoApp/app/src/main/java/com/example/legoapp/RegisterActivity.java
@@ -3,13 +3,22 @@ package com.example.legoapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -24,9 +33,42 @@ public class RegisterActivity extends AppCompatActivity {
             return insets;
         });
     }
-    public void changeView(View view) {
-        startActivity(new Intent(RegisterActivity.this, MainPageActivity.class));
-        Toast.makeText(this, "Welcome User", Toast.LENGTH_LONG).show();
+    public void changeLogInView(View view) {
+        startActivity(new Intent(RegisterActivity.this, LogInActivity.class));
+    }
+
+    public void addToFirebase_DB(View view) {
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        Map<String, Object> values = new HashMap<>();
+        TextView email = findViewById(R.id.editTextEmailRegister);
+        TextView password = findViewById(R.id.editTextPasswordReg);
+        TextView nickname = findViewById(R.id.editTextNickname);
+
+        values.put("email",email.getText().toString());
+        values.put("password",password.getText().toString());
+
+        // Añadir nuevo documento (habría que hacer un control de errores para comporbar si ya existe)
+        database.collection("users").document(nickname.getText().toString())
+                .set(values)
+                //
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Toast.makeText(RegisterActivity.this,"New User Registered!",Toast.LENGTH_LONG).show();
+                        changeMainView();
+                    }
+
+                    private void changeMainView() {
+                        startActivity(new Intent(RegisterActivity.this, MainPageActivity.class));
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(RegisterActivity.this,"ERROR",Toast.LENGTH_LONG).show();
+
+                    }
+                });
     }
 
 }
