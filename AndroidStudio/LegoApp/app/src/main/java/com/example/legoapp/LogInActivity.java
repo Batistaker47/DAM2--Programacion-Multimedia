635c4310b82a644951c4c4887aeae1644bab0bca
batistaker47 <david.pires1997@gmail.com>
2024-10-30 13:48:30 +0100

    DataBaseAux class created

--- AndroidStudio/LegoApp/app/src/main/java/com/example/legoapp/LogInActivity.java
+++ AndroidStudio/LegoApp/app/src/main/java/com/example/legoapp/LogInActivity.java
@@ -1,6 +1,8 @@
 package com.example.legoapp;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -18,9 +20,6 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class LogInActivity extends AppCompatActivity {
 
@@ -41,7 +40,7 @@ public class LogInActivity extends AppCompatActivity {
 
     public void logIn(View view) {
         FirebaseFirestore database = FirebaseFirestore.getInstance();
-        Map<String, Object> values = new HashMap<>();
+        //Map<String, Object> values = new HashMap<>();
         TextView nickname = findViewById(R.id.editTextNicknameSignIn);
         TextView password = findViewById(R.id.editTextPasswordSignIn);
 
@@ -62,7 +61,9 @@ public class LogInActivity extends AppCompatActivity {
                                     // && database.collection("users").document("password").toString().equals(password.getText().toString())
                                     if (doc.getId().equals(nickname.getText().toString()) && doc.get("password").toString().equals(password.getText().toString())) {
                                         // Si existe y la contraseña coincide
+                                        String email = doc.get("email").toString();
                                         userCheck = true;
+                                        createLocalRegistry(email,nickname.getText().toString(),password.getText().toString());
                                         Toast.makeText(LogInActivity.this, "Welcome back, " + nickname.getText().toString() + "!", Toast.LENGTH_LONG).show();
                                         startActivity(new Intent(LogInActivity.this, MainPageActivity.class));
                                         break;
@@ -79,4 +80,27 @@ public class LogInActivity extends AppCompatActivity {
                     });
             }
     }
+
+    private void createLocalRegistry(String email, String nickname, String password) {
+        if(email == null || nickname == null ||password == null)
+            return;
+        DataBaseAux dbAux = new DataBaseAux(this);
+        SQLiteDatabase db = dbAux.getWritableDatabase();
+        db.execSQL("INSERT INTO users VALUES (" + email + "," + nickname + "," + password + ")");
+    }
+
+    private boolean checkLocalDatabase() {
+        DataBaseAux dbAux = new DataBaseAux(this);
+        SQLiteDatabase db = dbAux.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM users", null);
+        if (cursor.moveToFirst()) {
+            String email = cursor.getString(1);
+            String nickname = cursor.getString(2);
+            String password = cursor.getString(3);
+            return true;
+        }
+
+        return false;
+    }
+
 }
