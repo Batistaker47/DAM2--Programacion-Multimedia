04e2800013de7b5e5387e110b52f4f83fce31909
batistaker47 <david.pires1997@gmail.com>
2024-11-06 14:13:16 +0100

    Interfaz y Game manager implementado

--- Unity/TestPong/Assets/Scripts/Goals.cs
+++ Unity/TestPong/Assets/Scripts/Goals.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class Goals : MonoBehaviour
 {
+    public uint playerIndex;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Ball ball = collision.GetComponent<Ball>();
         if (ball)
         {
             ball.ResetPosition();
+
+            int currentScore = GameManager.instance.GetIndexScore((int)playerIndex);
+
+            GameManager.instance.SetIndexScore((int)playerIndex, currentScore + 1);
         }
     }
 }
